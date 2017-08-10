1a3d28f20e593adda357cf6c79f5034a62f65b14
Michał <kwestorowski.m@gmail.com>
2017-08-10 22:36:29 +0200

    Making architecture of project

---
+++ src/state/reducer.js
@@ -0,0 +1,12 @@
+import productsSearching from './parts/productsSearching'
+
+
+
+export default (state = {}, action) => {
+    return {
+        productsSearching: productsSearching(state.productsSearching, action),
+        products: products(state.products, action),
+
+
+    }
+}
