2c9f97c62115f7cdb2543ead5a3312b171c071fe
Michał <kwestorowski.m@gmail.com>
2017-08-12 22:28:55 +0200

    Maked nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,34 @@
 import React from 'react'
 import {
+    Navbar,
+    Button,
+    FormGroup,
+    FormControl
 
 } from 'react-bootstrap'
 
 
 
-export default class  extends React.component {
+export default class Navigation extends React.Component {
     render() {
         return(
-            <div></div>
+            <Navbar>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="#">Brand</a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Navbar.Form pullLeft>
+                        <FormGroup>
+                            <FormControl type="text" placeholder="Znajdz swojego stażyste" />
+                        </FormGroup>
+                        {' '}
+                        <Button type="submit">Submit</Button>
+                    </Navbar.Form>
+                </Navbar.Collapse>
+            </Navbar>
 
         )
     }
--- src/state/reducer.js
+++ src/state/reducer.js
@@ -1,11 +1,11 @@
-import productsSearching from './parts/productsSearching'
+// import productsSearching from './parts/productsSearching'
 
 
 
 export default (state = {}, action) => {
     return {
-        productsSearching: productsSearching(state.productsSearching, action),
-        products: products(state.products, action),
+        // productsSearching: productsSearching(state.productsSearching, action),
+        // products: products(state.products, action),
 
 
     }
