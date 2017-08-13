38ecddbad5763f0e4dbde1a0aec2373acb37079a
Michał <kwestorowski.m@gmail.com>
2017-08-13 22:31:21 +0200

    Making form for creating account

---
+++ src/components/FirebaseConfig.js
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: "AIzaSyBEsyoqvY955QsdrdbEpGIWvNNnvZ3PC0U",
+    authDomain: "koty-app-login.firebaseapp.com",
+    databaseURL: "https://koty-app-login.firebaseio.com",
+    projectId: "koty-app-login",
+    storageBucket: "",
+    messagingSenderId: "364801817856"
+};
+
+export default config
